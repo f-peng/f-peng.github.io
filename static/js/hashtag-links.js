53324ba30806289a6d7aef39448db7ca6cc09e8b
f-peng <58820044+f-peng@users.noreply.github.com>
2025-07-06 11:34:46 +0200

    Apply news-link styling to all links on relevant pages

---
+++ static/js/hashtag-links.js
@@ -0,0 +1,13 @@
+var path = window.location.pathname;
+var addAnews = path.indexOf('/blog/cyberwatch/') === 0 || path === '/privacy/';
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('a').forEach(function(a){
+    if (addAnews) {
+      a.classList.add('anews-link');
+    }
+    if (a.textContent.trim().startsWith('#')) {
+      a.classList.add('hashtag-link');
+    }
+  });
+});
